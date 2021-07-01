32f25cfb299bf85cc88150ecbce57c396d9ea6c1
tadiwaIV <tadiwachivhere@gmail.com>
2021-07-01 19:12:01 +0200

    Adding Content

---
+++ src/Components/MainContent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import {Container, Row, Col, Button, Card, Form} from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faMobileAlt } from "@fortawesome/free-solid-svg-icons";
+
+const MainContent = () => (
+  <div>
+    <h1> Main Content</h1>
+  </div>
+);
+
+export default MainContent;
